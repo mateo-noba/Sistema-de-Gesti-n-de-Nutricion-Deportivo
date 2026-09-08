9d345a85fb242e0a3506ea112c4631834c0a767a
mateonoba <mateob.nobafioriti@gmail.com>
2026-09-08 09:19:52 -0300

    8-9-26 9:18 Mateo Noba
    Agrege una nueva carpeta para poner las pantallas del clinete y comence a hacer la pantalla de home del cliente

---
+++ src/paginas/Cliente/Home.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import {View, Text, StyleSheet, TouchableOpacity,TextInput } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
+import { RootStackParamList } from "../../../App";
+
+type HomeScreenProp = NativeStackNavigationProp<RootStackParamList, "Home">;
+const Home = () =>{
+
+  const navigation = useNavigation<HomeScreenProp>();
+
+  return(
+    <View style={styles.container}>
+        <View style={styles.contenedorHome}>
+            <Text style={styles.titulo}>Nombre apellido</Text>
+    
+        </View>
+    </View>
+
+
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#4db6ac",
+    padding: 20,
+    alignItems: "center",
+  },
+  titulo: {
+    textAlign: "center",
+    marginBottom: 25,
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#333",
+    marginTop: 100,
+  },
+  contenedor: {
+    flexDirection: "row",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    gap: 25,
+  },
+  card: {
+    width: 230,
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+    borderRadius: 12,
+    alignItems: "center",
+    margin: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  icono: {
+    fontSize: 45,
+    marginBottom: 10,
+  },
+  tituloCard: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  descripcion: {
+    textAlign: "center",
+    marginTop: 5,
+    color: "#333",
+  },
+  contenedorHome: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    width: 330,
+    height: 100,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 12,
+    padding: 10,
+    backgroundColor: "#fafafa",
+    width: 250,
+  },
+  boton: {
+    backgroundColor: "#4db6ac",
+    padding: 12,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  contenedorFormulario:{
+    marginTop: 50,
+  },
+  textoInicioSesion:{
+    marginTop: 10,
+  },
+  textoChico:{
+    fontSize: 11,
+    marginBottom: 30,
+  },
+  link:{
+    color: "#0004d8"
+
+  }
+});
+
+export default Home;
