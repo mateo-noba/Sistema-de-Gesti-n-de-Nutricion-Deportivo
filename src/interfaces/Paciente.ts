48948a31ec89a9a79b6a83d40773bda355c5bade
Ivan Arevalos <ivanarevalos27@gmail.com>
2026-09-22 08:50:14 -0300

    Creada la interfaz de Paciente

---
+++ src/interfaces/Paciente.ts
@@ -0,0 +1,11 @@
+export interface Paciente {
+  id?: string;
+  nombre: string;
+    apellido: string;
+    dni: string;
+    email: string;
+    telefono: string;
+    peso:number;
+    altura:number;
+    imc:number;
+}
